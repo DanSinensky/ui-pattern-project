67a90262ede8c4c698119d1c000ee3ca89d8e14d
Dan Sinensky <dan.i.sinensky@gmail.com>
2023-07-06 12:03:21 -0400

    positions divs, including flag array

--- script.js
+++ script.js
@@ -7,70 +7,88 @@ const bars = document.querySelectorAll(".bar")
 const hamburger = document.querySelector(".hamburger")
 const aside = document.querySelector("aside")
 const flags = document.querySelector(".flags")
-
-class Country {
-  constructor(flagIcon, officialName, commonName, capital, borders, continents, subregion, flags, languages) {
-    this.flagIcon = flagIcon
-    this.officialName = officialName
-    this.commonName = commonName
-    this.capital = capital
-    this.borders = borders
-    this.continents = continents
-    this.subregion = subregion
-    this.flags = flags
-    this.languages = languages
-  }
-}
+const common = document.querySelector(".common")
+const official = document.querySelector(".official")
+const img = document.querySelector("img")
+const capital = document.querySelector(".capital")
+const borders = document.querySelector(".borders")
+const subregion = document.querySelector(".subregion")
 
 form.addEventListener("submit", e => {
   e.preventDefault()
   fetch(`${baseUrl}/name/${e.target.elements.country.value}`)
-  .then(res => {
-    console.log("success", res)
-    return res.json()
-  })
+    .then(res => {
+      console.log("success", res)
+      return res.json()
+    })
     .then(data => {
       const country = data[0]
       displayUI(country)
     })
-  .catch(error => {
-    console.error("Something went wrong...", error)
-  })
+    .catch(error => {
+      console.error("Something went wrong...", error)
+    })
   e.target.elements.country.value = ""
 })
 
 randomButton.addEventListener("click", e => {
   fetch(`${baseUrl}/all`)
-  .then(res => {
-    console.log("success", res)
-    return res.json()
-  })
-  .then(data => {
-    const country = data[Math.floor(Math.random()*data.length)]
-    displayUI(country)
-  })
-  .catch(error => {
-    console.error("Something went wrong...", error)
-  })
+    .then(res => {
+      console.log("success", res)
+      return res.json()
+    })
+    .then(data => {
+      const country = data[Math.floor(Math.random() * data.length)]
+      displayUI(country)
+    })
+    .catch(error => {
+      console.error("Something went wrong...", error)
+    })
 })
 
 function displayUI(country) {
-  let html = `
-      <div class="country">
-        <h2>${country.name.common}</h2>
-        <h3>${country.name.official}</h3>
-        <img src="${country.flags.png}" alt="Flag of ${country.name.common}">
-        <p>${country.capital}</p>
-        <p>${country.borders}</p>
-        <p>${country.continents}</p>
-        <p>${country.subregion}</p>
-      </div>`
+  common.innerText = country.name.common
+  official.innerText = country.name.official
+  img.src = country.flags.png
+  img.alt = `Flag of ${country.name.common}`
+  capital.innerText = country.capital
+  if (country.borders === undefined) {
+    borders.innerText = "Island"
+  } else {
+    borders.innerText = country.borders
+  }
+  subregion.innerText = country.subregion
 
-  main.insertAdjacentHTML("afterBegin", html)
-  
-  let flag = `${country.flag}`
-  flags.innerText = flag + flags.innerText
-}
+  let flag = document.createElement("p")
+  flag.classList.add("flag-icon")
+  flag.dataset.commonName = country.name.common
+  flag.innerText = country.flag
+  flags.appendChild(flag)
+  flag.addEventListener("click", e => {
+    fetch(`${baseUrl}/name/${flag.dataset.commonName}`)
+    .then(res => {
+      console.log("success", res)
+      return res.json()
+    })
+    .then(data => {
+      const country = data[0]
+      common.innerText = country.name.common
+      official.innerText = country.name.official
+      img.src = country.flags.png
+      img.alt = `Flag of ${country.name.common}`
+      capital.innerText = country.capital
+      if (country.borders === undefined) {
+        borders.innerText = "Island"
+      } else {
+        borders.innerText = country.borders
+      }
+      subregion.innerText = country.subregion
+    })
+    .catch(error => {
+      console.error("Something went wrong...", error)
+    })
+    })
+  }
 
 bars.forEach(bar => {
   bar.addEventListener("click", e => {
