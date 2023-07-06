21a354cb0acce9046918d36eb06b8dc1e016426c
Dan Sinensky <dan.i.sinensky@gmail.com>
2023-07-06 12:44:09 -0400

    no longer duplicates flags

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const img = document.querySelector("img")
 const capital = document.querySelector(".capital")
 const borders = document.querySelector(".borders")
 const subregion = document.querySelector(".subregion")
+const flagArray = []
 
 form.addEventListener("submit", e => {
   e.preventDefault()
@@ -47,23 +48,28 @@ randomButton.addEventListener("click", e => {
 })
 
 function displayUI(country) {
-  common.innerText = country.name.common
-  official.innerText = country.name.official
-  img.src = country.flags.png
-  img.alt = `Flag of ${country.name.common}`
-  capital.innerText = country.capital
-  if (country.borders === undefined) {
-    borders.innerText = "Island"
-  } else {
-    borders.innerText = country.borders
+  const print = country => {
+    common.innerText = country.name.common
+    official.innerText = country.name.official
+    img.src = country.flags.png
+    img.alt = `Flag of ${country.name.common}`
+    capital.innerText = country.capital
+    if (country.borders === undefined) {
+      borders.innerText = "Island"
+    } else {
+      borders.innerText = country.borders
+    }
+    subregion.innerText = country.subregion
   }
-  subregion.innerText = country.subregion
-
+  print(country)
   let flag = document.createElement("p")
   flag.classList.add("flag-icon")
   flag.dataset.commonName = country.name.common
   flag.innerText = country.flag
-  flags.appendChild(flag)
+  if (flagArray.includes(country.flag) === false) {
+    flags.appendChild(flag)
+    flagArray.push(country.flag)
+  }
   flag.addEventListener("click", e => {
     fetch(`${baseUrl}/name/${flag.dataset.commonName}`)
     .then(res => {
@@ -72,23 +78,13 @@ function displayUI(country) {
     })
     .then(data => {
       const country = data[0]
-      common.innerText = country.name.common
-      official.innerText = country.name.official
-      img.src = country.flags.png
-      img.alt = `Flag of ${country.name.common}`
-      capital.innerText = country.capital
-      if (country.borders === undefined) {
-        borders.innerText = "Island"
-      } else {
-        borders.innerText = country.borders
-      }
-      subregion.innerText = country.subregion
+      print(country)
     })
     .catch(error => {
       console.error("Something went wrong...", error)
     })
-    })
-  }
+  })
+}
 
 bars.forEach(bar => {
   bar.addEventListener("click", e => {
